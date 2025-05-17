290e843e08c199f7ce406cc7879bd76ad7499c1d
obarra <obarra2023@alu.uct.cl>
2025-05-17 18:21:40 -0400

    Algoritmo para evaluar convexidad en un intervalo dado

--- 02.Programación no lineal/main.py
+++ 02.Programación no lineal/main.py
@@ -35,8 +35,22 @@ def es_concava(fun_original,rango_lmd,rango_x):
                 return mensaje
     return mensaje
 
+def es_convexa(fun_original,rango_lmd,rango_x):
+    mensaje = f"la función: {fun_original} SI es Convexa en el intervalo ({rango_x[0]:.2f}, {rango_x[-1]:.2f})."
+    for lmd in rango_lmd:
+        for i in range(1, len(rango_x)):
+            valor_pivote = valor_funcion_pivote(fun_original,lmd,rango_x,i)
+            valor_fun_org_lmd = valor_funcion_original_con_lambda(fun_original,lmd,rango_x,i)
+            restriccion_concavidad = valor_fun_org_lmd >= valor_pivote
+            if (not restriccion_concavidad):
+                mensaje = f"la función: {fun_original} NO es Convexa en el intervalo ({rango_x[0]:.2f}, {rango_x[-1]:.2f})."
+                return mensaje
+    return mensaje
+
 def evaluar_convexidad_y_concavidad(obj,fun_original,rango_lmd,rango_x):
-    if obj == "concava":
+    if obj == "convexa":
+        return es_convexa(fun_original,rango_lmd,rango_x)
+    elif obj == "concava":
         return es_concava(fun_original,rango_lmd,rango_x)
     else:
         mensaje = "Porfavor ingresar un objetivo valido: 'convexa' o 'concava'"
