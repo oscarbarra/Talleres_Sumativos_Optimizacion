cbe2f8eb17d35acc956fe4cf8b4d46f2e5a9c633
obarra <obarra2023@alu.uct.cl>
2025-05-19 19:47:14 -0400

    implementación de un identificador en caso que la función tienda al infinito

--- 03.Programación no lineal/Derivada numerica/main.py
+++ 03.Programación no lineal/Derivada numerica/main.py
@@ -12,12 +12,12 @@ def calcular_atol(valor1, valor2, epsilon=1e-8):
     max_val = max(abs(valor1), abs(valor2))
     return epsilon * max_val
 
-def valor_fun_original(fun_original,valor_x):
+def valor_fun_x(fun_original,valor_x):
     local_dict = {"x":valor_x, **fun_especiales}
     evaluacion = parse_expr(fun_original, local_dict=local_dict, transformations=transformaciones, evaluate=True)
     return evaluacion
 
-def valor_fun_modificada(fun_original,valor_x,h):
+def valor_fun_xh(fun_original,valor_x,h):
     variables  = {"x":valor_x + h}
     local_dict = {**variables, **fun_especiales}
     evaluacion = parse_expr(fun_original, local_dict=local_dict, transformations=transformaciones, evaluate=True)
@@ -30,14 +30,19 @@ def derivada_numerica(fun_original,valor_x,delta_x,margen_eval=1e-6):
     derivada = [None, None]
     lados_eval = [lado_der, lado_izq]
 
-    fun_x = valor_fun_original(fun_original,valor_x)
+    fun_x = valor_fun_x(fun_original,valor_x)
     for i in range(2):
-        fun_xh = valor_fun_modificada(fun_original,valor_x,lados_eval[i])
+        fun_xh = valor_fun_xh(fun_original,valor_x,lados_eval[i])
         try:
             derivada[i] = float(((fun_xh - fun_x) / lados_eval[i]).evalf())
         except AttributeError:
             derivada[i] = (fun_xh - fun_x) / lados_eval[i]
-    
+        except TypeError:
+            fun_derivada = f"({fun_xh}-1*({fun_x}))/{lados_eval[i]}"
+            mensaje = f"Posible asintota detectada, la función: {fun_derivada}" + \
+                f" puede tener tendencia al infinito en el punto x = {valor_x}"
+            return mensaje
+        
     atol = calcular_atol(derivada[0],derivada[1],epsilon=1e-8)
     son_cercanos = isclose(derivada[0],derivada[1],rtol=1e-4,atol=atol)
     mensaje = f"La derivada de la función {fun_original} es: {derivada[0]}."
@@ -48,9 +53,9 @@ def derivada_numerica(fun_original,valor_x,delta_x,margen_eval=1e-6):
 
 def main():
     # Función a la que se la buscará su derivada
-    fun_original = "x^(1/3) * sin(1/x)"
+    fun_original = "1/x"
     # Valor de X
-    valor_x = 0
+    valor_x = 0.0
     # Valor al que tiende delta x
     delta_x = 0.0
     # Resultado de la evaluación
