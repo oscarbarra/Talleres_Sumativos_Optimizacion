d4e3b23ef1174851577d02212dc9e487d0b0500a
obarra <obarra2023@alu.uct.cl>
2025-05-26 18:40:01 -0400

    Simplificación de la logica de la deribada numerica

--- 03.Programación no lineal/Derivada_numerica/der_numerica.py
+++ 03.Programación no lineal/Derivada_numerica/der_numerica.py
@@ -1,48 +1,39 @@
 
-from numpy import isclose, pi
-from sympy import sin, cos, tan, cot, sec, csc, sqrt, log, E
-from sympy.parsing.sympy_parser import parse_expr, standard_transformations, \
-      convert_xor, implicit_multiplication_application
+from numpy import pi
+from sympy import sympify
 
-transformaciones = standard_transformations + (convert_xor,implicit_multiplication_application)
-fun_especiales = {"sin":sin,"cos":cos,"tan":tan,"cot":cot,"sec":sec,"csc":csc,\
-                  "sqrt":sqrt,"log":log,"ln":log,"e":E,"pi":pi}
-
-def valor_fun_x(fun_original,x):
-    local_dict = {"x":x, **fun_especiales}
-    evaluacion = parse_expr(fun_original, local_dict=local_dict, transformations=transformaciones, evaluate=True)
+def valor_fun_x(fun,x):
+    evaluacion = sympify(fun).subs({"x":x})
     return evaluacion
 
-def valor_fun_xh(fun_original,x,h):
-    variables  = {"x":x + h}
-    local_dict = {**variables, **fun_especiales}
-    evaluacion = parse_expr(fun_original, local_dict=local_dict, transformations=transformaciones, evaluate=True)
+def valor_fun_xh(fun,x,h):
+    evaluacion = sympify(fun).subs({"x": x+h})
     return evaluacion
 
-def derivada_numerica(fun_original,x,h):
-    fun_x = valor_fun_x(fun_original,x)
-    fun_xh = valor_fun_xh(fun_original,x,h)
+def derivada_numerica(fun,x,h):
+    fun_x = valor_fun_x(fun,x)
+    fun_xh = valor_fun_xh(fun,x,h)
     try:
         derivada = float(((fun_xh - fun_x) / h).evalf())
     except AttributeError:
         derivada = (fun_xh - fun_x) / h
     except TypeError:
         err = "err"
-        mensaje = f"Derivada de la función {fun_original} NO esta definida en el punto: x={x}"
+        mensaje = f"Derivada de la función {fun} NO esta definida en el punto: x={x}"
         return (mensaje, err)
         
-    mensaje = f"La derivada de la función {fun_original} es: {derivada}."
+    mensaje = f"La derivada de la función {fun} es: {derivada}."
     return (mensaje, derivada)
 
 def main():
     # Función a la que se la buscará su derivada
-    fun_original = "1/(x-2)"
+    fun = "1/(x-2)"
     # Valor de X
-    x = 0
+    x = 2
     # Valor al que tiende delta x
     h = 1e-8
     # Resultado de la evaluación
-    resultado = derivada_numerica(fun_original,x,h)
+    resultado = derivada_numerica(fun,x,h)
     # Muestra el resultado por consola
     print(resultado[0])
     return
